b02d662ed56b53f6cdb6c2f447a1fdf8761e3cb8
M Ghaleb <msghaleb@users.noreply.github.com>
2021-02-23 15:28:00 +0100

    Update settings.py

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'applicationinsights.django.ApplicationInsightsMiddleware',
 ]
 
 ROOT_URLCONF = 'azuresite.urls'
@@ -121,3 +122,31 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+APPLICATION_INSIGHTS = {
+    # (required) Your Application Insights instrumentation key
+    'ikey': os.environ['APPINSKEY'],
+    
+    # (optional) By default, request names are logged as the request method
+    # and relative path of the URL.  To log the fully-qualified view names
+    # instead, set this to True.  Defaults to False.
+    'use_view_name': True,
+    
+    # (optional) To log arguments passed into the views as custom properties,
+    # set this to True.  Defaults to False.
+    'record_view_arguments': True,
+    
+    # (optional) Exceptions are logged by default, to disable, set this to False.
+    'log_exceptions': False,
+    
+    # (optional) Events are submitted to Application Insights asynchronously.
+    # send_interval specifies how often the queue is checked for items to submit.
+    # send_time specifies how long the sender waits for new input before recycling
+    # the background thread.
+    'send_interval': 1.0, # Check every second
+    'send_time': 3.0, # Wait up to 3 seconds for an event
+    
+    # (optional, uncommon) If you must send to an endpoint other than the
+    # default endpoint, specify it here:
+    'endpoint': "https://dc.services.visualstudio.com/v2/track",
+}
